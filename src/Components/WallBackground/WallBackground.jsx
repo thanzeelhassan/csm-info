ad5ffc53c073950c62e5bc105315b3c1fe0045bb
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-08-04 02:02:34 +0530

    Added white and black backgrounds

---
+++ src/Components/WallBackground/WallBackground.jsx
@@ -0,0 +1,26 @@
+import './WallBackground.css'
+import image1 from '../../assets/background/white-wall-bg.jpg'
+import image2 from '../../assets/background/black-flowers.jpg'
+import image3 from '../../assets/background/black-wall.jpg'
+
+import PropTypes from 'prop-types';
+
+const Background = ({heroCount}) => {
+  if(heroCount === 0)
+  {
+    return <img src={image1} className= 'background fade-in' alt="" />
+  }
+  if(heroCount === 1)
+  {
+    return <img src={image2} className= 'background fade-in' alt="" />
+  }
+  if(heroCount === 2)
+  {
+    return <img src={image3} className= 'background fade-in' alt="" />
+  }
+}
+Background.propTypes = {
+  heroCount: PropTypes.number.isRequired, // Add PropTypes validation
+};
+
+export default Background
--- src/Pages/Discussions.jsx
+++ src/Pages/Discussions.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import Navbar from '../Components/Navbar/Navbar'
+import WallBackground from "../Components/WallBackground/WallBackground"
+import './Discussions.css'
+
 const Discussions = () => {
   return (
     <div>
+      <WallBackground heroCount={1}/>
       <Navbar> </Navbar>
-      <h1>Discussions</h1>
-      <ul>
-        <li><a href="https://example.com">Link 1</a></li>
-        <li><a href="https://example.com">Link 2</a></li>
-        <li><a href="https://example.com">Link 3</a></li>
+      <h1 className='h1-class'>Discussions</h1>
+
+      <h2 className='h1-class'>r/ChainsawMan</h2>
+      
+      <ul className='ul-class'>
+        <li className='li-class'><a href="https://www.reddit.com/r/ChainsawMan/comments/a2o6uw/disc_chainsawman_ch_1/" className='a-class'>Chapter 1</a></li>
+        <li className='li-class'><a href="https://www.reddit.com/r/ChainsawMan/comments/a4utu9/disc_chainsawman_ch_2/" className='a-class'>Chapter 2</a></li>
+        <li className='li-class'><a href="https://www.reddit.com/r/ChainsawMan/comments/abazic/disc_chainsawman_ch_5/" className='a-class'>Chapter 5</a></li>
       </ul>
     </div>
   );
