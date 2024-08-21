8d58c9edd258bfa1813009536e7ab304cc4aaff4
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-08-21 23:52:53 +0530

    Added github sources

--- src/Pages/ColouredMangaPage/ColouredMangaPage.jsx
+++ src/Pages/ColouredMangaPage/ColouredMangaPage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../Components/Navbar/Navbar';
 import './ColouredMangaPage.css';
 import chainsawman_image2 from '../../assets/1296717_800px.jpg';
 import firepunch_image from '../../assets/firepunch1.jpg';
+import github_icon from '../../assets/github-icon/github-mark-white.svg';
 
 const ColouredMangaPage = () => {
   return (
@@ -14,11 +15,17 @@ const ColouredMangaPage = () => {
           <a href="https://wayexit995.github.io/CSM/Manga/Index.html">
             <img src={chainsawman_image2} alt="Chainsaw Man" className='image-class'/>
           </a>
+          <a href="https://github.com/wayexit995/CSM" target="_blank" rel="noopener noreferrer" className='github-link'>
+            <img src={github_icon} alt="GitHub" className='github-icon'/>
+          </a>
         </div>
         <div className='image-wrapper'>
           <p className='image-text'>Fire Punch</p>
           <a href="https://wayexit995.github.io/Fire_Punch/">
             <img src={firepunch_image} alt="Fire Punch" className='image-class'/>
+          </a>
+          <a href="https://github.com/wayexit995/Fire_Punch" target="_blank" rel="noopener noreferrer" className='github-link'>
+            <img src={github_icon} alt="GitHub" className='github-icon'/>
           </a>
         </div>
       </div>
