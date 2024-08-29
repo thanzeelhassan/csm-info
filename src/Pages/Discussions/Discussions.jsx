ef85a3ddde4da4ec4569aa2ff7ccc77cb58d0fdf
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-08-29 18:20:31 +0530

    Added actual values to json file.. Displayed that in the discussions page

--- src/Pages/Discussions/Discussions.jsx
+++ src/Pages/Discussions/Discussions.jsx
@@ -1,80 +1,81 @@
-import React from 'react'
-import Navbar from '../../Components/Navbar/Navbar'
-import './Discussions.css'
-import fakeData from '../../assets/MOCK_DATA.json'
-import { useTable } from 'react-table'
-import { useMemo } from 'react'
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import "./Discussions.css";
+import fakeData from "../../assets/MOCK_DATA.json";
+import DiscussionData from "../../assets/discussions/Chainsawman Subreddit/chainsawman_discussions.json";
+import { useTable } from "react-table";
+import { useMemo } from "react";
 
 const Discussions = () => {
-  const data = useMemo(() => fakeData, []);
-  const columns = useMemo(() => [
-    {
-      Header: "ID",
-      accessor: "id",
-    },
-    {
-      Header: "First Name",
-      accessor: "first_name",
-    },
-    {
-      Header: "Last Name",
-      accessor: "last_name",
-    },
-    {
-      Header: "Email",
-      accessor: "email",
-    },
-    {
-      Header: "Gender",
-      accessor: "gender",
-    },
-    {
-      Header: "University",
-      accessor: "university",
-    },
-  ], []);
+  const data = useMemo(() => DiscussionData, []);
+  const columns = useMemo(
+    () => [
+      {
+        Header: "Chapter Number",
+        accessor: "chapter number",
+      },
+      {
+        Header: "Chapter Title",
+        accessor: "chapter title",
+      },
+      {
+        Header: "Reddit Thread",
+        accessor: "reddit thread",
+      },
+      {
+        Header: "Volume",
+        accessor: "volume number",
+      },
+      {
+        Header: "Release Date",
+        accessor: "release date",
+      },
+      {
+        Header: "Arc",
+        accessor: "arc name",
+      },
+    ],
+    []
+  );
 
-  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({ columns, data });
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+    useTable({ columns, data });
 
   return (
-    <div className='discussion-background fade-in'>
+    <div className="discussion-background fade-in">
       <Navbar> </Navbar>
-      <h1 className='discussion-h1-class'>Discussions</h1>
+      <h1 className="discussion-h1-class">Discussions</h1>
 
-      <h2 className='discussion-h1-class'>r/ChainsawMan</h2>
-      
-      <div className='discussion-container'>
+      <h2 className="discussion-h1-class">r/ChainsawMan</h2>
+
+      <div className="discussion-container">
         <table {...getTableProps()}>
           <thead>
-              {headerGroups.map((headerGroup) => (
-                <tr {...headerGroup.getHeaderGroupProps()}>
-                  {headerGroup.headers.map((column) => (
-                    <th {...column.getHeaderProps}>
-                      {column.render("Header")}
-                    </th>
-                  ))}
-                </tr>
-              ))}
+            {headerGroups.map((headerGroup) => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map((column) => (
+                  <th {...column.getHeaderProps}>{column.render("Header")}</th>
+                ))}
+              </tr>
+            ))}
           </thead>
 
           <tbody {...getTableBodyProps()}>
             {rows.map((row) => {
-              prepareRow(row)
+              prepareRow(row);
               return (
                 <tr {...row.getRowProps()}>
                   {row.cells.map((cell) => (
-                    <td {...cell.getCellProps()}>
-                      {cell.render("Cell")}
-                    </td>
+                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
                   ))}
                 </tr>
-              )
+              );
             })}
           </tbody>
         </table>
       </div>
     </div>
   );
-}
+};
 
 export default Discussions;
