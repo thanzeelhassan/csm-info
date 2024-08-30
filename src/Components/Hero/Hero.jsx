0fc33dd0602f7ed5967b7acddef4b7e764e64acf
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-08-30 17:00:54 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,36 +1,50 @@
-import './Hero.css'
-import arrow_btn from '../../assets/button/arrow_btn.png'
-import PropTypes from 'prop-types';
+import "./Hero.css";
+import arrow_btn from "../../assets/button/arrow_btn.png";
+import PropTypes from "prop-types";
 
-const Hero = ({heroData, setHeroCount, heroCount}) => {
+const Hero = ({ heroData, setHeroCount, heroCount }) => {
   return (
-    <div className='hero'>
-        <div className="hero-text">
-          <p>{heroData.text1}</p>
-          <p>{heroData.text2}</p>
-        </div>
-        <div className="hero-explore">
-          <p>Explore</p>
-          <img src={arrow_btn} alt="" />
-        </div>
-        <div className="hero-dot-play">
-          <ul className='hero-dots'>
-            <li onClick={()=> setHeroCount(0)} className={heroCount===0?"hero-dot orange":"hero-dot"}></li>
-            <li onClick={()=> setHeroCount(1)} className={heroCount===1?"hero-dot orange":"hero-dot"}></li>
-            <li onClick={()=> setHeroCount(2)} className={heroCount===2?"hero-dot orange":"hero-dot"}></li>
-            <li onClick={()=> setHeroCount(3)} className={heroCount===3?"hero-dot orange":"hero-dot"}></li>
-            <li onClick={()=> setHeroCount(4)} className={heroCount===4?"hero-dot orange":"hero-dot"}></li>
-          </ul>
-          <div className='hero-play'>
-          </div>
-        </div>
+    <div className="hero">
+      <div className="hero-text">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore</p>
+        <img src={arrow_btn} alt="" />
+      </div>
+      <div className="hero-dot-play">
+        <ul className="hero-dots">
+          <li
+            onClick={() => setHeroCount(0)}
+            className={heroCount === 0 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(1)}
+            className={heroCount === 1 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(2)}
+            className={heroCount === 2 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(3)}
+            className={heroCount === 3 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+          <li
+            onClick={() => setHeroCount(4)}
+            className={heroCount === 4 ? "hero-dot orange" : "hero-dot"}
+          ></li>
+        </ul>
+        <div className="hero-play"></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 Hero.propTypes = {
   heroCount: PropTypes.number.isRequired, // Add PropTypes validation
   heroData: PropTypes.array.isRequired,
-  setHeroCount: PropTypes.func.isRequired
+  setHeroCount: PropTypes.func.isRequired,
 };
 
-export default Hero
+export default Hero;
