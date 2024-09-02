40de780df211b91e504f7174c6331f7bce9bcd86
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-09-02 23:34:54 +0530

    Remove Unnecessary things

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,29 +1,29 @@
-import './Navbar.css'
-import { Link } from 'react-router-dom';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className='nav'>
-      <div className='nav-logo'>CSM Info</div>
-      <ul className='nav-menu'>
+    <div className="nav">
+      <div className="nav-logo">CSM Info</div>
+      <ul className="nav-menu">
         <li>
-          <Link to="/csm-info" className="nav-link">Home</Link>
+          <Link to="/csm-info" className="nav-link">
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/csm-info/explore" className="nav-link">Explore</Link>
-        </li> 
-        <li>
-          <Link to="/csm-info/about" className="nav-link">About</Link>
+          <Link to="/csm-info/discussions" className="nav-link">
+            Discussions
+          </Link>
         </li>
         <li>
-          <Link to="/csm-info/discussions" className="nav-link">Discussions</Link>
-        </li>
-        <li>
-          <Link to="/csm-info/colouredManga" className="nav-link">Coloured Manga</Link>
+          <Link to="/csm-info/colouredManga" className="nav-link">
+            Coloured Manga
+          </Link>
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
