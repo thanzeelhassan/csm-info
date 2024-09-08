b5e2c8bfc7418957a4e717f76d778afb0a7d7c98
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-09-08 16:05:07 +0530

    Added lots of data for all the chapters

--- src/Pages/Discussions/Discussions.jsx
+++ src/Pages/Discussions/Discussions.jsx
@@ -4,6 +4,12 @@ import "./Discussions.css";
 import Introduction_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/1. Introduction_Arc.json";
 import Bat_Devil_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/2. Bat Devil Arc.json";
 import Eternity_Devil_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/3. Eternity Devil Arc.json";
+import Katana_Man_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/4. Katana Man Arc.json";
+import Bomb_Girl_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/5. Bomb Girl Arc.json";
+import International_Assassins_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/6. International Assassins Arc.json";
+import Gun_Devil_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/7. Gun Devil Arc.json";
+import Control_Devil_Arc_Data from "../../assets/discussions/Chainsawman Subreddit/Part 1. Public Safety Saga/8. Control Devil Arc.json";
+
 import { useMemo } from "react";
 import DiscussionTable from "../../Components/DiscussionTable/DiscussionTable"; // Import the new component
 
@@ -11,6 +17,14 @@ const Discussions = () => {
   const introductionArcData = useMemo(() => Introduction_Arc_Data, []);
   const batDevilArcData = useMemo(() => Bat_Devil_Arc_Data, []);
   const eternityDevilArcData = useMemo(() => Eternity_Devil_Arc_Data, []);
+  const katanaManArcData = useMemo(() => Katana_Man_Arc_Data, []);
+  const bombGirlArcData = useMemo(() => Bomb_Girl_Arc_Data, []);
+  const internationalAssassinsArcData = useMemo(
+    () => International_Assassins_Arc_Data,
+    []
+  );
+  const gunDevilArcData = useMemo(() => Gun_Devil_Arc_Data, []);
+  const controlDevilArcData = useMemo(() => Control_Devil_Arc_Data, []);
 
   return (
     <div>
@@ -52,23 +66,23 @@ const Discussions = () => {
 
         <div>
           <h1 className="discussion-h2-class">Katana Man Arc</h1>
-          <DiscussionTable data={eternityDevilArcData} />
+          <DiscussionTable data={katanaManArcData} />
         </div>
         <div>
           <h1 className="discussion-h2-class">Bomb Girl Arc</h1>
-          <DiscussionTable data={eternityDevilArcData} />
+          <DiscussionTable data={bombGirlArcData} />
         </div>
         <div>
           <h1 className="discussion-h2-class">International Assassins Arc</h1>
-          <DiscussionTable data={eternityDevilArcData} />
+          <DiscussionTable data={internationalAssassinsArcData} />
         </div>
         <div>
           <h1 className="discussion-h2-class">Gun Devil Arc</h1>
-          <DiscussionTable data={eternityDevilArcData} />
+          <DiscussionTable data={gunDevilArcData} />
         </div>
         <div>
           <h1 className="discussion-h2-class">Control Devil Arc</h1>
-          <DiscussionTable data={eternityDevilArcData} />
+          <DiscussionTable data={controlDevilArcData} />
         </div>
       </div>
     </div>
