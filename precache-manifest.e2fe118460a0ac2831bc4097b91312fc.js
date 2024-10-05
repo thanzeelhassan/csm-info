ee131e38063206675c74e857bb252a5c78038ed0
Thanzeel Hassan <thanzeelhassan@gmail.com>
2024-10-05 14:56:11 +0530

    Updates

---
+++ precache-manifest.e2fe118460a0ac2831bc4097b91312fc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bfa306703ed41a9be513d87bf4c99b7",
+    "url": "/csm-info/index.html"
+  },
+  {
+    "revision": "97fa25e8c7e34d507ff7",
+    "url": "/csm-info/static/css/main.7be34f11.chunk.css"
+  },
+  {
+    "revision": "84b2c4bb0e3106197a38",
+    "url": "/csm-info/static/js/2.9d60c934.chunk.js"
+  },
+  {
+    "revision": "b95080709ac12d0967db165d90e6c04a",
+    "url": "/csm-info/static/js/2.9d60c934.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a223b0b288703caffa54",
+    "url": "/csm-info/static/js/3.35ad91b6.chunk.js"
+  },
+  {
+    "revision": "97fa25e8c7e34d507ff7",
+    "url": "/csm-info/static/js/main.49445530.chunk.js"
+  },
+  {
+    "revision": "0f0fb4ee8438ccd0c0c0",
+    "url": "/csm-info/static/js/runtime-main.f1cae79c.js"
+  },
+  {
+    "revision": "14d01a18ba4820e99000d0b255468f77",
+    "url": "/csm-info/static/media/1296717_800px.14d01a18.jpg"
+  },
+  {
+    "revision": "830390c4af9b606e7a0f33eed9c24396",
+    "url": "/csm-info/static/media/Angel_devil.830390c4.png"
+  },
+  {
+    "revision": "13dce76ef4787ff890487f33e394f54a",
+    "url": "/csm-info/static/media/Makima_eyes.13dce76e.jpg"
+  },
+  {
+    "revision": "3f8f3fc1f92d1ea33654b62469b1d2a7",
+    "url": "/csm-info/static/media/Weapons.3f8f3fc1.jpeg"
+  },
+  {
+    "revision": "67e0cb1b5c89786759eeeec0e8718903",
+    "url": "/csm-info/static/media/firepunch1.67e0cb1b.jpg"
+  },
+  {
+    "revision": "a0b00583d93c2f7084ad151ee0849934",
+    "url": "/csm-info/static/media/github-mark-white.a0b00583.svg"
+  }
+]);
